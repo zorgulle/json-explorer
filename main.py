166b2e74fa98778886352ee84f3462f9e838a656
Adrien <winy95battle@gmail.com>
2018-12-15 11:47:28 +0100

    Add some features

--- main.py
+++ main.py
@@ -2,14 +2,42 @@ class Node:
     def __init__(self, node_type, node_value):
         self.node_type = node_type
         self.node_value = node_value
+        self.children = []
 
-    def __str__(self):
-        return "%s(%s)" % (self.node_type.upper(), self.node_value)
+
+
+    def get_dict(self):
+        return {self.node_type: self.node_value}
+
+    def get_value(self, node_type):
+        if self.node_type == node_type:
+            return self.node_value
+
+
+
+class NodeWithChildren(Node):
+    def add_child(self, node):
+        self.children.append(node)
+
+    def get_dict(self):
+        result = {}
+        for child in self.children:
+            result.update({child.node_type: child.node_value})
+
+        return result
+
+    def get_value(self, node_type):
+        result = []
+        for child in self.children:
+            child.get_value(node_type)
+
+        return result
 
 def construct(dict_to_explore):
+    root_node = NodeWithChildren("root", "root")
     for node_type, node_value in dict_to_explore.items():
-        node = Node(node_type, node_value)
-    return node
+        root_node.add_child(Node(node_type, node_value))
+    return root_node
 
 if __name__ == '__main__':
     print("Main")
--- test_case.py
+++ test_case.py
@@ -1,8 +1,36 @@
 import pytest
+import unittest
 from main import construct
 
-def test_simple_json():
-    simple_json = {"name": "Boris"}
-    
-    node = construct(simple_json)
-    assert node.__str__() == "NAME(Boris)"
+class TestParsingAndDictConstruction(unittest.TestCase):
+    def test_simple_dict(self):
+        simple_dict = {"name": "Boris"}
+
+        node = construct(simple_dict)
+        self.assertDictEqual(node.get_dict(), simple_dict)
+
+    def test_mutiple_type_same_level(self):
+        simple_dict = {
+            "name": "Boris",
+            "nickname": "hardbass"
+        }
+
+        node = construct(simple_dict)
+        self.assertDictEqual(node.get_dict(), simple_dict)
+
+    def test_dict_with_list_int_value(self):
+        simple_dict = {
+            "toto": [1, 2, 3]
+        }
+        node = construct(simple_dict)
+        self.assertDictEqual(node.get_dict(), simple_dict)
+
+    def test_dict_with_subdict(self):
+        simple_dict = {
+            "level1": {
+                "level2": "You Win"
+            }
+        }
+        node = construct(simple_dict)
+        self.assertListEqual(node.get_value("level2"), ["You Win"])
+
